29c2485988b5d2cfc424e9966593f284eb9fcfd0
Munezero26200 <alinemunezero920@gmail.com>
2025-07-01 16:50:14 +0200

    Here is simple change on style of my todo app

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-const form= document.getElementById("todo-form");
-const input= document.getElementById("todo-input");
-const list= document.getElementById("todo-list");
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
 
-form.addEventListener("submit",event=>{
-   event.preventDefault();//stop page reload.
-   const taskText=input.value.trim();
-   if(taskText !== ""){
-     addTask(taskText);
-     input.value="";//clear input
-   }
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const taskText = input.value.trim();
+    if (taskText !== "") {
+        addTask(taskText);
+        input.value = "";
+    }
 });
-const addTask= text=>{
-    const li= document.createElement("li");
-    li.className="flex items-center justify-center bg-gray-100 px-4 py-y2 rounded-lg";
+const addTask = text => {
+    const li = document.createElement('li');
+    li.className = "flex items-center justify-between bg-green-100 px-4 py-2 rounded-2xl"
 
-    const span= document.createElement("span");
-    span.textContent=text;
-    //Mark task as completed on click
+    const span = document.createElement("span");
+    span.textContent = text;
+    span.className = "cursor-pointer"
+    //mark a completed task
     span.addEventListener("click", () => {
-    span.classList.toggle("line-through");
-    span.classList.toggle("text-gray-500");
-  });
-
+        span.classList.toggle("line-through");
+        span.classList.toggle("text-gray-500");
+    });
     //delete button
-    const deleteBtn= document.createElement("button");
-    deleteBtn.innerHTML="🗑️";
-    deleteBtn.className="ml-4 text-red-500"
-    deleteBtn.addEventListener("click", ()=>{
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "🗑️";
+    deleteBtn.className = "ml-4"
+    deleteBtn.addEventListener('click', () => {
         li.remove();
     });
-    //put span and button inside list item
     li.appendChild(span);
     li.appendChild(deleteBtn);
     list.appendChild(li);
-}
+};
